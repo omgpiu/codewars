be5bab4c2089b80d7c6953b12ed6dd0e02de3558
omgpiu <dyba@list.ru>
2021-03-20 20:07:43 +0300

    init

--- codewars.js
+++ codewars.js
@@ -25,19 +25,48 @@
 
 //return middle character (s)
 
-function getMiddle(s) {
-    let result = s.split('')
-    if (result.length === 1) return result[0]
+// function getMiddle(s) {
+//     let result = s.split('')
+//     if (result.length === 1) return result[0]
+//
+//     if (result.length % 2 === 0) {
+//         return result[(result.length / 2) - 1] + result[result.length / 2]
+//     } else {
+//         return result[Math.trunc(result.length / 2)]
+//     }
+//
+// }
+//
+//function getMiddle(s)
+// {
+//   return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
+// }
+// console.log(getMiddle("testing"))
 
-    if (result.length % 2 === 0) {
-        return result[(result.length / 2) - 1] + result[result.length / 2]
-    } else {
-        return result[Math.trunc(result.length / 2)]
+//Descending Order
+// function descendingOrder(n) {
+//     let a = n.toString().split('')
+//     if (a.length === 1) {
+//         return Number(a[0])
+//     }
+//     return Number(a.sort((a, b) => (b - a)).join(''))
+//
+// }
+//function descendingOrder(n){
+//     return parseInt(String(n).split('').sort().reverse().join(''))
+// }
+// console.log(descendingOrder(123456789))
+function isIsogram(str) {
+    let a = str.toLowerCase().split('')
+    if (a.length <= 1) return true
+
+    if (a.length > Array.from(new Set(a)).length) {
+        return false
     }
-
+    return true
 }
 
-
-console.log(getMiddle("testing"))
-
+ console.log(
+isIsogram("aba")
+ )
 
