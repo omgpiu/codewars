bea01676eed911dba5a720a1cf96945d3eb99287
omgpiu <dyba@list.ru>
2021-03-21 17:04:35 +0300

    catMouse

--- codewars.js
+++ codewars.js
@@ -72,15 +72,34 @@
 // function isIsogram(str){
 //     return new Set(str.toUpperCase()).size == str.length;
 // }
-function calc(x) {
-    let array = x.split('')
-    let result = array.map(e => e.charCodeAt(0)).join('').split('')
-    let result2 = result.map(e => {
-        return e == 7 ? '1' : e
-    })
-    return Number(result.join('')) - Number(result2.join(''))
-}
-
-console.log(
-    calc('ABC')
-)
+// function calc(x){
+//     let sum = n => [...n].reduce((a,b) => +a + +b);
+//     let total1 = x.replace(/./g, x => x.charCodeAt(0));
+//     let total2 = total1.replace(/7/g,'1');
+//     return sum(total1) - sum(total2);
+// }
+//
+//
+// function calc(x) {
+//     let result = x.split('').map(e => e.charCodeAt(0)).join('').split('')
+//     let result2 = result.map(e => {
+//         return e == 7 ? '1' : e
+//     })
+//     let summ = result.reduce((acc, value) => Number(acc) + Number(value), [])
+//     let sum2 = result2.reduce((acc, value) => Number(acc) + Number(value), [])
+//
+//     return summ - sum2
+// }
+//
+// console.log(
+//     calc('aaaaaddddr')
+// )
+// function catMouse(map,moves){
+//     map=map.split('\n')
+//     if (!map.some(v=>v.includes('C'))||!map.some(v=>v.includes('m'))) return 'boring without two animals'
+//     let catX=map.map(v=>v.indexOf('C')).filter(v=>v>-1)*1
+//     let catY=map.map(v=>v.includes('C')).indexOf(true)+1
+//     let mX=map.map(v=>v.indexOf('m')).filter(v=>v>-1)*1
+//     let mY=map.map(v=>v.includes('m')).indexOf(true)+1
+//     return Math.abs(catX-mX)+Math.abs(catY-mY)<=moves?'Caught!':'Escaped!'
+// }
