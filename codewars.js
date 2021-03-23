a8b7fb8b27e884b1a4d722d25563507fa00b9f00
omgpiu <dyba@list.ru>
2021-03-23 19:24:36 +0300

    isAnagram

--- codewars.js
+++ codewars.js
@@ -268,3 +268,8 @@
 //     return a === line.toString() ? true : false;
 // }
 // console.log(isPalindrome(12321))
+function isAnagram(test, original) {
+    return test.toLowerCase().split("").sort().join("") === original.toLowerCase().split("").sort().join("");
+}
+
+console.log(isAnagram("foefet", "toffee"))
